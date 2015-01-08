f9ae1f64772a5d0d2e18ff21ab51877803aab5c0
Artur Grigio <fsastudent@yahoo.com>
2015-01-08 11:26:23 -0800

    fixed index.php
    fixed index.php to accept database as well

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
 	if (is_array($files))
 		$files = implode(" ",$files);
 	**********************************************/	
-
-	$fp = popen('zip -r - 1.jpg 2.jpg 3.jpg', 'r');
+	if ($files)
+		$fp = popen('zip -r - '.$files, 'r');
+	else
+		$fp = popen('zip -r - 1.jpg 2.jpg 3.jpg', 'r');
 	$bufsize = 8192;
 	$buff = '';
 	while( !feof($fp) ) {
