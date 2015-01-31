dcf3f9167d431badc9c685426647ef1caf49180a
Artur Grigio <fsastudent@yahoo.com>
2015-01-30 16:43:06 -0800

    final commit
    Got a complain that my "else" should have curlys, if my "if" had them

--- index.php
+++ index.php
@@ -15,8 +15,9 @@
 		(string)$files; //without static casting into string, the escape characters were messing with the buffer (probably cutting off earlier)
 		$fp = popen('zip -r - '.$files, 'r');
 	}
-	else
+	else{
 		$fp = popen('zip -r - 1.jpg 2.jpg 3.jpg', 'r');
+	}
 		
 	$bufsize = 8192; //apperently max buffer size is 8192 before cutoff
 	$buff = '';
