39f7d5df5674e7d2f49f0d29f02a8e9488ca1b1d
Artur Grigio <fsastudent@yahoo.com>
2015-10-07 23:31:17 -0700

    Buffer need flush() before usage
    If the buffer wasn't flushed, sometimes the pipeline would terminate
    before completing.

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php 
 	header('Content-disposition: attachment; filename="myZip.zip"');
 	header('Content-Type: application/octet-stream');
+	header('Content-Length: 5000000') // You will have to either pass a variable to Content-Length or assign a value in bytes
 
 	//if using a MySQL database
 	/**********************************************
@@ -12,18 +13,19 @@
 		$files = implode(" ",$files);
 	**********************************************/	
 	if ($files){
-		(string)$files; //without static casting into string, the escape characters were messing with the buffer (probably cutting off earlier)
 		$fp = popen('zip -r - '.$files, 'r');
 	}
 	else{
 		$fp = popen('zip -r - 1.jpg 2.jpg 3.jpg', 'r');
 	}
-		
+	
+	flush() //flushing the buffer once brfore using	
 	$bufsize = 8192; //apperently max buffer size is 8192 before cutoff
 	$buff = '';
 	while( !feof($fp) ) {
 	   $buff = fread($fp, $bufsize);
 	   echo $buff;
+	   flush() //flusing the buffer afte reach fread
 	}
 	pclose($fp);
 ?>
