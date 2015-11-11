209a9e4117051d3f7939027f3b5da551d87ff1b8
Artur Grigio <fsastudent@yahoo.com>
2015-11-10 23:16:16 -0800

    Changes to Content-Length
    For some reason, content-length doesn't get sent if you do an addition
    inside it. Might have something to do "header() must be called before
    any actual output is sent" in
    http://php.net/manual/en/function.header.php

--- index.php
+++ index.php
@@ -6,23 +6,24 @@
 	foreach ($files as $file) {
 		$totalSize += filesize($file);
 	}
+	$totalSize += 300; //I don't understand why, but the totalSize is always 300kb short of the correct size
 
 	header('Pragma: no-cache'); 
 	header('Content-Description: File Download'); 
 	header('Content-disposition: attachment; filename="myZip.zip"');
 	header('Content-Type: application/octet-stream');
-	header('Content-Length: ' . $totalSize+300); //I don't understand why, but the total length is always 300kb short of the correct size
+	header('Content-Length: ' . $totalSize);
 	header('Content-Transfer-Encoding: binary'); 
 
 	//Opening a zip stream
 	$files = implode(" ", $files);
 	if ($files){
-		$fp = popen('zip -r -0 - '.$files, 'r');
+		$fp = popen('zip -r -0 - ' . $files, 'r');
 	}
 	
 	flush(); //Flushing the butter, pre streaming
 	while(!feof($fp)) {
-	   echo fread($fp, 8192);
+	   	echo fread($fp, 8192);
 	}
 
 	//Closing the stream
